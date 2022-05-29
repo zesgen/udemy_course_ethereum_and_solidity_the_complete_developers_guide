0b85863fc529112ac3c65b7959991472f4acbeb4
Evgenii Zaitsev <zesgen@gmail.com>
2022-05-30 00:09:04 +0700

    Improve the compilation and deploy scripts

--- inbox/compile.js
+++ inbox/compile.js
@@ -5,5 +5,10 @@ const solc = require('solc')
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
 const source = fs.readFileSync(inboxPath, 'utf8')
 
-//console.log(solc.compile(source, 1))
-module.exports = solc.compile(source, 1).contracts[':Inbox']
+const compileResult = solc.compile(source, 1)
+if (compileResult.hasOwnProperty('errors')) {
+    console.log('Compilation error:')
+    console.log(compileResult)
+    process.exit(1)
+}
+module.exports = compileResult.contracts[':Inbox']
--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 const Web3 = require('web3')
-const {contractInterface, bytecode} = require('./compile')
+const compileResult = require('./compile')
 const MNEMONIC = '<use_yours>'
 const INFURA_API_URL = '<use_yours>'
 const provider = new HDWalletProvider(MNEMONIC, INFURA_API_URL)
@@ -9,8 +9,8 @@ const web3 = new Web3(provider)
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts()
     console.log('Attempting to deploy from account: ', accounts[0])
-    const result = await new web3.eth.Contract(JSON.parse(contractInterface))
-        .deploy({data: bytecode, arguments: ['Hi there!']})
+    const result = await new web3.eth.Contract(JSON.parse(compileResult.interface))
+        .deploy({data: compileResult.bytecode, arguments: ['Hi there!']})
         .send({gas: '1000000', from: accounts[0]})
     console.log('Contract deployed to ', result.options.address)
     provider.engine.stop()
