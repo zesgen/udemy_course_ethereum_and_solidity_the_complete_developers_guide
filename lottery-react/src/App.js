3f9cc6f94d15209681bc2a6de3ed35ee8d5a50ef
Evgenii Zaitsev <zesgen@gmail.com>
2022-05-30 20:30:53 +0700

    Improve the app

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,29 +1,88 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import web3 from './web3'
 import lottery from "./lottery";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+    state = {
+        manager: '',
+        players: [],
+        balance: '',
+        value: '',
+        message: ''
+    };
 
-    this.state = {manager: ''};
-  }
+    async componentDidMount() {
+        const manager = await lottery.methods.manager().call();
+        const players = await lottery.methods.getPlayers().call();
+        const balance = await web3.eth.getBalance(lottery.options.address);
 
-  async componentDidMount() {
-    const manager = await lottery.methods.manager().call();
+        this.setState({manager, players, balance});
+    }
 
-    this.setState({manager: manager});
-  }
+    onSubmit = async (event) => {
+        event.preventDefault();
 
-  render() {
-    return (
-        <div>
-          <h2>Lottery Contract</h2>
-          <p>This contract is managed by {this.state.manager}</p>
-        </div>
-    );
-  }
+        const accounts = await web3.eth.getAccounts();
+
+        this.setState({message: 'Waiting on transaction success...'})
+
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, 'ether')
+            });
+            this.setState({message: 'You have been entered!'});
+        } catch (e) {
+            this.setState({message: 'Something went wrong! Try again'});
+        }
+    };
+
+    onPickWinner = async (event) => {
+        event.preventDefault();
+
+        const accounts = await web3.eth.getAccounts();
+
+        this.setState({message: 'Waiting on transaction success...'})
+
+        try {
+            await lottery.methods.pickWinner().send({
+                from: accounts[0]
+            });
+            this.setState({message: 'The winner has been picked!'});
+        } catch (e) {
+            this.setState({message: 'Something went wrong! Try again'});
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Lottery Contract</h2>
+                <p>
+                    This contract is managed by {this.state.manager}
+                    There are currently {this.state.players.length} people entered,
+                    to competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+                </p>
+                <hr/>
+                <form onSubmit={this.onSubmit}>
+                    <h4>Want to try your luck?</h4>
+                    <div>
+                        <label>Amount of ether to enter: </label>
+                        <input
+                            value={this.state.value}
+                            onChange={event => this.setState({value: event.target.value})}/>
+                    </div>
+                    <button>Enter</button>
+                </form>
+                <hr/>
+                <h4>Ready to pick a winner?</h4>
+                <button onClick={this.onPickWinner}>Pick a winner!</button>
+                <hr/>
+                <h1>{this.state.message}</h1>
+            </div>
+        );
+    }
 }
+
 export default App;
